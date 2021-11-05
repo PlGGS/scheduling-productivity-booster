29963ee2ae4d7347a35e642bd6b6e0a18c1e1f15
PlGGS <bbor0422@gmail.com>
2021-11-05 18:25:50 -0500

    Actually fixed build error by removing double passed defaultValue property in calendar

--- components/calendar.js
+++ components/calendar.js
@@ -177,8 +177,7 @@ class Calendar extends Component {
                         <label>{constants.CALENDAR_INTERVAL_LENGTH}</label>
                         <Select defaultValue={this.state.slotStep}
                                 onChange={(value) => this.handleIntervalLength(value)}
-                                style={{width: "220px", paddingBottom: '10px'}}
-                                defaultValue="15 mins">
+                                style={{width: "220px", paddingBottom: '10px'}}>
                             <Option value="1">15 mins </Option>
                             <Option value="2">30 mins</Option>
                             <Option value="3">45 mins</Option>
