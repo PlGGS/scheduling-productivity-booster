c4917aa237cb2e75daac82a536c827b5b8bcf309
PlGGS <bbor0422@gmail.com>
2021-11-08 17:28:42 -0600

    I swear please build feedback page

--- pages/feedback.js
+++ pages/feedback.js
@@ -54,9 +54,9 @@ const Feedback = (props) => {
           }}
         >
           {allUsers.map((user, index) => (
-            <Grid item xs={12} sm={2} md={3}>
+            <Grid key={user.firstname} item xs={12} sm={2} md={3}>
               <div className="gridItem">
-                <Paper>
+                <Paper key={user.firstname}>
                   <h2 className="username">
                     {user.firstname} {user.lastname}
                   </h2>
@@ -64,7 +64,7 @@ const Feedback = (props) => {
                     <div className="dropdown">
                       <Dropdown
                         placeholder="Select a badge"
-                        key={index}
+                        key={user.firstname}
                         options={["Scribe", "Leader", "Mediator", "Creative", "Positive"]}
                         value=""
                         onChange={(value) => console.log("change!", value)}
