d05e2dad14239ddb58722fc91c099dad8c137998
PlGGS <bbor0422@gmail.com>
2021-11-12 05:04:38 -0600

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,38 @@
-import React from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Collection from '../components/firebase/collection'
-import Link from 'next/link'
-import Layout from '../components/layout'
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Collection from "../components/firebase/collection";
+import Link from "next/link";
+import Layout from "../components/layout";
 
 class Index extends React.Component {
   render() {
     return (
       <Layout>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Scheduling Productivity Booster
-          </h1>
-          <br />
-          <p className={styles.description}>
-            By: Saiyed Irfanullah, Isaiah Antonio, Kuiper Poznyak, Ming Zhang, David Shargorodsky, Blake Boris{' '}
-            <br />
-            <br />
-          </p>
-        </main>
-      </div>
+        <div className={styles.container}>
+          <div className="contain">
+            <main className={styles.main}>
+              <h1 className={styles.title}>Scheduling Productivity Booster</h1>
+              <br />
+              <p className={styles.description}>
+                By: Saiyed Irfanullah, Isaiah Antonio, Kuiper Poznyak, Ming
+                Zhang, David Shargorodsky, Blake Boris <br />
+                <br />
+              </p>
+            </main>
+          </div>
+        </div>
+        <style>{`
+       .contain {
+          height: 90vh;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          text-align: center;
+       }
+      `}</style>
       </Layout>
-    )
+    );
   }
 }
 
