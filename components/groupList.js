3c920fe49747239a0a83da424341654c8cb4b8a7
PlGGS <bbor0422@gmail.com>
2021-11-12 05:05:55 -0600

    Fixed grouplist three lines bug Updated layout

--- components/groupList.js
+++ components/groupList.js
@@ -1,38 +1,44 @@
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import Link from './link';
-import { IconContext } from 'react-icons';
-import { ObjectCollection } from './firebase/objectCollection';
-import { onSnapshot, collection } from '@firebase/firestore';
-import { db } from '../services/firebase';
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import Link from "./link";
+import { IconContext } from "react-icons";
+import { ObjectCollection } from "./firebase/objectCollection";
+import { onSnapshot, collection } from "@firebase/firestore";
+import { db } from "../services/firebase";
 
-function GroupList({...props}) {
+function GroupList({ ...props }) {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
-  
+
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='navbar'>
-          <Link href='/dashboard' className='menu-bars'>
+      <IconContext.Provider value={{ color: "#000" }}>
+        <div className="navbar">
+          <Link href="/dashboard" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
         </div>
-        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle'>
-              <Link href='/dashboard' className='menu-bars'>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link href="/dashboard" className="menu-bars">
                 <AiIcons.AiOutlineClose />
               </Link>
             </li>
             {props.workgroups.map((item, index) => {
               return (
-                <li key={index} className={'text-' + index}>
-                  <Link href='/dashboard' className='menu-item' >
-                    <span onClick={() => props.setWorkgroup(item.name)}>{item.name === props.workgroup ? <b>{item.name}</b> : item.name}</span>
+                <li key={index} className={"text-" + index}>
+                  <Link href="/dashboard" className="menu-item">
+                    <span onClick={() => props.setWorkgroup(item.name)}>
+                      {item.name === props.workgroup ? (
+                        <b>{item.name}</b>
+                      ) : (
+                        item.name
+                      )}
+                    </span>
                   </Link>
                 </li>
               );
@@ -41,84 +47,83 @@ function GroupList({...props}) {
         </nav>
       </IconContext.Provider>
       <style jsx>{`
-            .navbar {
-              background-color: #fff;
-              height: 80px;
-              display: flex;
-              justify-content: start;
-              align-items: center;
-            }
-            
-            .menu-bars {
-              margin-left: 2rem;
-              font-size: 2rem;
-              background: none;
-            }
-            
-            .nav-menu {
-              background-color: #fff; //change this later
-              width: 250px;
-              height: 100vh;
-              display: flex;
-              justify-content: center;
-              position: fixed;
-              top: 100px;
-              left: -100%;
-              transition: 800ms;
-              border-style: solid;
-              
-            }
-            
-            .nav-menu.active {
-              left: 0;
-              transition: 350ms;
-            }
-            
-            .nav-text {
-              display: flex;
-              justify-content: start;
-              align-items: center;
-              padding: 8px 0px 8px 16px;
-              list-style: none;
-              height: 60px;
-            }
-            
-            .nav-text a {
-              text-decoration: none;
-              color: #f5f5f5;
-              font-size: 18px;
-              width: 95%;
-              height: 100%;
-              display: flex;
-              align-items: center;
-              padding: 0 16px;
-              border-radius: 4px;
-            }
-            
-            .nav-text a:hover {
-              background-color: #1a83ff;
-            }
-            
-            .nav-menu-items {
-              width: 100%;
-            }
-            
-            .navbar-toggle {
-              background-color: #fff;
-              width: 100%;
-              height: 42px;
-              display: flex;
-              justify-content: start;
-              align-items: center;
-              transform: translate(-19px);
-            }
-            
-            span {
-              margin-left: 16px;
-            }
-        `}</style>
+        .navbar {
+          margin-right: 0px;
+          display: flex;
+          justify-content: start;
+          align-items: center;
+          transform: translate(2px, -5px);
+        }
+
+        .menu-bars {
+          margin-left: 2rem;
+          font-size: 2rem;
+          background: none;
+        }
+
+        .nav-menu {
+          background-color: #fff; //change this later
+          width: 250px;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          position: fixed;
+          top: 90px;
+          left: -100%;
+          transition: 800ms;
+          border-style: solid;
+        }
+
+        .nav-menu.active {
+          left: 0;
+          transition: 350ms;
+        }
+
+        .nav-text {
+          display: flex;
+          justify-content: start;
+          align-items: center;
+          padding: 8px 0px 8px 16px;
+          list-style: none;
+          height: 60px;
+        }
+
+        .nav-text a {
+          text-decoration: none;
+          color: #f5f5f5;
+          font-size: 18px;
+          width: 95%;
+          height: 100%;
+          display: flex;
+          align-items: center;
+          padding: 0 16px;
+          border-radius: 4px;
+        }
+
+        .nav-text a:hover {
+          background-color: #1a83ff;
+        }
+
+        .nav-menu-items {
+          width: 100%;
+        }
+
+        .navbar-toggle {
+          background-color: #fff;
+          width: 100%;
+          height: 42px;
+          display: flex;
+          justify-content: start;
+          align-items: center;
+          transform: translate(-30px);
+        }
+
+        span {
+          margin-left: 16px;
+        }
+      `}</style>
     </>
   );
-}  
+}
 
 export default GroupList;
--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,36 @@
-import Head from 'next/head';
-import Header from './header';
-import Footer from './footer';
+import Head from "next/head";
+import Header from "./header";
+import Footer from "./footer";
 
 const Layout = (props) => (
-    <div>
-       <Head>
-          <title>Scheduling Productivity Booster</title>
-          <meta name="DePaul University CSC394 Scheduler" content="By: Saiyed Irfanullah, Isaiah Antonio, Kuiper Poznyak, Nathan Santiago, David Shargorodsky, Blake Boris" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Header/>
-        <div id="front">
-            {props.children}
-        </div>
-        <Footer />
+  <div>
+    <Head>
+      <title>Scheduling Productivity Booster</title>
+      <meta
+        name="DePaul University CSC394 Scheduler"
+        content="By: Saiyed Irfanullah, Isaiah Antonio, Kuiper Poznyak, Nathan Santiago, David Shargorodsky, Blake Boris"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Header />
+    <div className="wrapper">
+      <div id="front">{props.children}</div>
     </div>
+    <style jsx>{`
+      .front {
+        height: 100%;
+      }
+    //   .wrapper {
+    //     border-style: solid;
+    //     border-color: yellow;
+    //     grid-template-columns: repeat(3, 1fr);
+    //     grid-template-rows: repeat(2, 1fr);
+    //     grid-gap: 10px;
+    //     width: 100%;
+    //     height: 100%;
+    //   }
+    `}</style>
+  </div>
 );
 
 export default Layout;
