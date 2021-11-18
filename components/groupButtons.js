bd67f861ee6c90bc9a6f340d0d0b85832d24a549
PlGGS <bbor0422@gmail.com>
2021-11-18 15:40:32 -0600

    Got join group button working Stopped overwriting previously created workgroups

--- components/groupButtons.js
+++ components/groupButtons.js
@@ -1,4 +1,4 @@
-import { doc, updateDoc, addDoc, collection, Timestamp } from "@firebase/firestore";
+import { doc, setDoc, updateDoc, addDoc, collection, Timestamp, arrayUnion, getDoc } from "@firebase/firestore";
 import { auth, db, provider } from "../services/firebase";
 import { React, useState } from "react";
 import { Modal, Button } from "react-bootstrap";
@@ -6,38 +6,72 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function GroupButtons({ ...props }) {
   const [newGroupName, setNewGroupName] = useState("");
+  const [groupNameToJoin, setGroupNameToJoin] = useState("");
   const [showNewGroupModal, setShowNewGroupModal] = useState(false);
-  const [showJoinGroupModal, setJoinNewGroupModal] = useState(false);
+  const [showJoinGroupModal, setShowJoinGroupModal] = useState(false);
 
-  const handleClose = () => setShowNewGroupModal(false);
-  const handleShow = () => setShowNewGroupModal(true);
+  const handleNewShow = () => setShowNewGroupModal(true);
+  const handleNewClose = () => setShowNewGroupModal(false);
+
+  const handleJoinShow = () => setShowJoinGroupModal(true);
+  const handleJoinClose = () => setShowJoinGroupModal(false);
 
   async function createNewGroup() {
     if (newGroupName !== "") {
+      //Check if group already exists, so we don't overwrite its admin and datecreated
+      const group = doc(db, "workgroup", newGroupName);
+      const docExists = await getDoc(group);
+      if (docExists.exists()) {
+        alert("Already exists");
+      }
+      else {
+        await setDoc(doc(db, "workgroup", newGroupName), {
+          name: newGroupName,
+          datecreated: Timestamp.now(),
+          admin: props.user.uid,
+          members: [props.user]
+        });
+      }
       //Add group to workgroups collection with a list of 
       // users in that group containing the user that just created the group
-      await addDoc(collection(db, "workgroup"), {
-        name: newGroupName,
-        datecreated: Timestamp.now(),
-        members: [props.user]
-      });
       //switch the dashboard to that group
       props.setWorkgroup(newGroupName);
       setShowNewGroupModal(false);
+      setNewGroupName("");
+    }
+  }
+
+  async function joinGroup() {
+    if (groupNameToJoin !== "") {
+      //Add group to workgroups collection with a list of 
+      // users in that group containing the user that just created the group
+      const group = doc(db, "workgroup", groupNameToJoin);
+      console.log(group);
+      await updateDoc(group, {
+        members: arrayUnion(props.user)
+      }).then((status) => {
+        alert("Successfully added to " + groupNameToJoin)
+      }).catch((error) => {
+        alert("Fail");
+      });
+      //switch the dashboard to that group
+      props.setWorkgroup(groupNameToJoin);
+      setShowJoinGroupModal(false);
+      setGroupNameToJoin("");
     }
   }
 
   return (
     <>
-      <Button className="newGroupBtn" variant="primary" style={{width: "50%"}} onClick={handleShow}>
+      <Button className="newGroupBtn" variant="primary" style={{width: "50%"}} onClick={handleNewShow}>
         New Group
       </Button>
-      <Button className="joinGroupBtn" variant="secondary" style={{width: "50%"}} onClick={handleShow}>
+      <Button className="joinGroupBtn" variant="secondary" style={{width: "50%"}} onClick={handleJoinShow}>
         Join Group
       </Button>
-      <Modal show={showNewGroupModal} onHide={handleClose}>
+      <Modal show={showNewGroupModal} onHide={handleNewClose}>
         <Modal.Header>
-          <Modal.Title>New Group</Modal.Title>
+          <Modal.Title>New group</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <input
@@ -48,11 +82,32 @@ function GroupButtons({ ...props }) {
           />
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleNewClose}>
             Close
           </Button>
           <Button variant="primary" onClick={createNewGroup}>
             Create
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showJoinGroupModal} onHide={handleJoinClose}>
+        <Modal.Header>
+          <Modal.Title>Join a new group</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input
+            className="newGroupNameInput"
+            value={groupNameToJoin}
+            onChange={(e) => setGroupNameToJoin(e.target.value)}
+            placeholder="Group name"
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleJoinClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={joinGroup}>
+            Join
           </Button>
         </Modal.Footer>
       </Modal>
