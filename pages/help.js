36fa62dd89795049f2faf9b46fcfede4ae3b7c48
PlGGS <bbor0422@gmail.com>
2021-11-22 16:47:52 -0600

    Added tutorial video to help page Added our depaul.edu emails to contact page

--- pages/help.js
+++ pages/help.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Collection from '../components/firebase/collection'
-import Link from 'next/link'
-import Layout from '../components/layout'
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Collection from "../components/firebase/collection";
+import Link from "next/link";
+import Layout from "../components/layout";
 
 class Index extends React.Component {
   render() {
     return (
       <Layout>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Tutorial Video
-          </h1>
-          <br />
-          {/* Place embedded video here */}
-        </main>
-      </div>
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <h1 className={styles.title}>Tutorial Video</h1>
+            <br />
+            <iframe
+              width="853.3333333333334"
+              height="480"
+              src="https://www.youtube.com/embed/g8m56D6VKKo"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </main>
+        </div>
       </Layout>
-    )
+    );
   }
 }
 
